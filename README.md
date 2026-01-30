beb897718ae18a705cff6a0b98b8c30e5c02bf66
Erick L. <ecklpz@gmail.com>
2026-01-30 10:29:27 -0500

    fix(01-day): k8s config for ci

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ### Bootcamp
 
 - [Day 0: Repository planning, architecture definition, and cost awareness.](docs/advanced-bootcamp/00-day/README.md)
+- [Day 1: EKS & GitHub Actions.](docs/advanced-bootcamp/00-day/README.md)
 
--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -19,19 +19,42 @@ module "eks" {
 
   name               = "${var.project_name}-cluster"
   kubernetes_version = "1.31"
-  region             = var.aws_region
+  vpc_id             = module.vpc.vpc_id
+  subnet_ids         = module.vpc.private_subnets
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  endpoint_public_access                   = true
+  endpoint_private_access                  = true
+  endpoint_public_access_cidrs             = ["0.0.0.0/0"]
+  enable_cluster_creator_admin_permissions = true
+
+  access_entries = {
+    github_actions = {
+      principal_arn = "arn:aws:iam::711387135481:role/aw-bootcamp-gh-role"
+      policy_associations = {
+        admin = {
+          policy_arn   = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = { type = "cluster" }
+        }
+      }
+    }
+  }
 
   eks_managed_node_groups = {
-    deafult = {
+    default = {
       instance_types = [var.node_instance_type]
       min_size       = 1
       max_size       = 1
       desired_size   = 1
     }
+  }
 
+  enable_irsa = true
+
+  # Addons as a map
+  addons = {
+    vpc-cni    = { version = "v1.13.0-eksbuild.1" }
+    coredns    = { version = "v1.23.0-eksbuild.1" }
+    kube-proxy = { version = "v1.31.0-eksbuild.1" }
   }
 
   tags = var.tags
